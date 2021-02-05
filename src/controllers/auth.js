8b9031d5d4543d35b8b072bb3bfe7fb268d3febe
Ahmed M. Hamed <ahmedhamedd2017@gmail.com>
2021-02-05 17:17:18 +0200

    added login endpoint

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,9 @@
 
 const userModel = require('../models/user');
 
-module.exports.signup = ((req,res,next) => {
-    const newUser = new userModel(req.body);
-    newUser.save()
+module.exports.signup = (async(req,res,next) => {
+    await userModel
+    .create(req.body)
     .then((result) => {
         res.status(201).send("User created!");
     })
@@ -11,3 +11,18 @@ module.exports.signup = ((req,res,next) => {
         console.log(error);
     })
 });
+
+module.exports.login = (async(req,res,next) => {
+    await userModel
+    .find(req.body)
+    .then((result) => {
+        if(result){
+            res.status(200).send(`Welcome back, ${req.body.username}`);
+        }else{
+            res.status(401).send("Wrong credentials!");
+        }
+    })
+    .catch((error) => {
+
+    })
+})
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,7 @@ const auth = require('../controllers/auth');
 
 const router = express.Router();
 
-router.use('/signup', auth.signup);
+router.post('/signup', auth.signup);
+router.post('/login',auth.login)
 
 module.exports = router;
