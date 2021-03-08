449934b904454de0018b3d1d247a0c1afcf295cb
Ahmed M. Hamed <ahmedhamedd2017@gmail.com>
2021-03-08 15:51:36 +0200

    optimized the signup process to hash passwords/ added fields in user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,14 +1,42 @@
+const bcrypt = require('bcrypt');
+const user = require('../models/user');
 
 const userModel = require('../models/user');
 
 module.exports.signup = (async(req,res,next) => {
     await userModel
-    .create(req.body)
-    .then((result) => {
-        res.status(201).send("User created!");
+    .findOne({email : req.body.email})
+    .then(async(rec) => {
+        if(rec){
+            return res.status(409).json({
+                message: "User already exists!"
+            })
+        }else{
+            bcrypt.hash(req.body.password, 10 , async(error, hash) => {
+                if(error){
+                    return res.status(500).json({
+                        message: error
+                    })
+                }else{
+                    const signupRequest = {...req.body, password: hash}
+                    await userModel
+                    .create(signupRequest)
+                    .then((result) => {
+                        res.status(201).send("User created!");
+                    })
+                    .catch((error) => {
+                        return res.status(500).json({
+                            message: error
+                        })
+                    })
+                }
+            })
+        }
     })
     .catch((error) => {
-        console.log(error);
+        return res.status(500).json({
+            message: error
+        })
     })
 });
 
@@ -19,10 +47,12 @@ module.exports.login = (async(req,res,next) => {
         if(result){
             res.status(200).send(`Welcome back, ${req.body.username}`);
         }else{
-            res.status(401).send("Wrong credentials!");
+            return res.status(401).send("Wrong credentials!");
         }
     })
     .catch((error) => {
-
+        return res.send(500).json({
+            message: error
+        })
     })
 })
--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username:{
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
         type: String,
         required: true
     },
     password:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    gender:{
         type: String,
         required: true
     }
